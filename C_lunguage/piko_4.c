681558fcae8230468be6e4aa24d84acf5d0bf5f7
tama <red.ze.tm@gmail.com>
2025-11-08 12:46:10 +0900

    コメント全処理に入れています（途中）

--- C_lunguage/piko_4.c
+++ C_lunguage/piko_4.c
@@ -7,15 +7,16 @@ int main(void) {
     pid_t pid;
     int status;
 
+    //親プロセスのPIDを表示する。
     printf("親プロセス: PID=%d\n", getpid());
 
-    pid = fork();
-    if (pid < 0) {
-        perror("fork 失敗");
-        exit(EXIT_FAILURE);
+    pid = fork();   //子プロセスの生成（ここから親子プロセスに分岐）
+    if (pid < 0) {  // fork失敗時の処理　PIDが負の値になる
+        perror("fork 失敗");    //errnoに基づくエラーメッセージを表示
+        exit(EXIT_FAILURE);    //EXIET_FAILURE(定義では1)で異常終了
     }
 
-    if (pid == 0) {
+    if (pid == 0) {     //fork()で子プロセスが受け取るPIDは0
         // 子プロセス
         printf("子プロセス: PID=%d, 親PID=%d\n", getpid(), getppid());
         execlp("ls", "ls", "-l", NULL);  // ls -l を実行
